9f89a5e5a40b195e2a17144c4cbc7ac5842f698e
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-10-20 12:30:00 +0800

    hero project can be cycled through no transition animation yet cycles back to i=0 when at the end of projects array

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+const Projects = [
+  {
+    title: "Car Rental Web Application",
+    img: "/images/project_car-rental-spa.png",
+    subtitle: "Full-Stack, Vanilla JS Single Page Application"
+  },
+  {
+    title: "Other Project",
+    img: "/images/project_placeholder.png",
+    subtitle: "This is the other project that will go here"
+  }
+];
+
+export { Projects };
--- src/page-controllers/home.js
+++ src/page-controllers/home.js
@@ -1,5 +1,6 @@
 import { App } from "./../components/App.js";
 import { Waves } from "./../components/Waves.js";
+import { Projects } from "./../components/Projects.js";
 //import { Notify } from "./../components/Notify.js";
 //import { Modal } from "./../components/Modal.js";
 
@@ -7,14 +8,51 @@ function homePageController() {
   // alert("this is the home page");
   let data = {
     intro: "I create meaningful, digital experiences",
-    project_title: "Car Rental Web Application",
-    project_subtitle: "Full-Stack, Vanilla JS Single Page Application"
+    project_title: null,
+    project_subtitle: null,
+    project_img: null
     //secondaryContent: "This is secondary content."
   };
   //console.log("homepage");
   App.loadPage("home", "template-page-home", data, () => {
     //console.log("home");
     Waves.init();
+
+    const nextBtn = document.createElement("h1");
+    nextBtn.innerHTML = "&#10140;";
+    nextBtn.classList.add("next-btn", "subtle-shadow");
+    document.querySelector(".landing-column_2").appendChild(nextBtn);
+    for (let index = 0; index < Projects.length; index++) {
+      let i = 0;
+      let project = Projects[i];
+      project["index"] = i;
+      //console.log(project);
+      setProject(project);
+      nextBtn.addEventListener("click", () => {
+        if (i < Projects.length - 1) {
+          i += 1;
+        } else {
+          i = 0;
+        }
+        project = Projects[i];
+        project["index"] = i;
+        console.log(project);
+        setProject(project);
+      });
+      return;
+    }
   });
+
+  function setProject(project) {
+    data.project_title = project.title;
+    data.project_subtitle = project.subtitle;
+    data.project_img = project.img;
+
+    document.querySelector("#hero-project-title").innerHTML =
+      data.project_title;
+    document.querySelector("#hero-project-subtitle").innerHTML =
+      data.project_subtitle;
+    document.querySelector(".hero-project-img").src = data.project_img;
+  }
 }
 export { homePageController };
