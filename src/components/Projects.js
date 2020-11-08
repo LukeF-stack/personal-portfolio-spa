10d449de5a35037ae5a5c50da4d4b652dd3d71b9
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-08 22:03:17 +0800

    car rental case photos implemented

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,12 @@ const Projects = [
     img: "/images/project_car-rental-spa.png",
     display_img: "/images/car_rental_display.png",
     subtitle: "Full-Stack, Vanilla JS Single Page Application",
+    case_img_1: "/images/car_rental_case1.png",
+    case_img_2: "/images/car_rental_case2.png",
+    link: {
+      link: "https://google.com",
+      link_title: "See live website here"
+    },
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu risus dictum ut ut id porttitor vel aliquet. Magna viverra nunc egestas risus aliquam, volutpat sed ac, libero. Tincidunt proin pellentesque pretium enim, sollicitudin vulputate ultrices scelerisque facilisis. Purus sollicitudin elit malesuada porta facilisis egestas imperdiet. Mi lorem consectetur nibh velit. Facilisi sit massa sagittis, malesuada ornare feugiat donec. Gravida leo etiam tincidunt ultrices. Vulputate nisl, enim lorem arcu, nibh viverra varius. Morbi aliquam nulla tellus, arcu, mauris urna purus dapibus et. In lectus nec venenatis amet.",
     icons: [
